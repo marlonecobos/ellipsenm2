0524bd2cbc74b79d3e315e2f8049783f7e496beb
marlonecobos <manubio13@gmail.com>
2024-04-23 15:41:03 -0500

    v0.3.5 subsample added as a type of replicate

--- R/data_subsample.R
+++ R/data_subsample.R
@@ -1,4 +1,4 @@
-#' Helper function to subsample occurrence data
+#' Helper function to sub-sample occurrence data
 #'
 #' @param data data.frame of occurrence records. Columns must be longitude and
 #' latitude.
@@ -7,14 +7,14 @@
 #' equal the number of records in \code{data}. Default = 1 returns the complete
 #' set of data as the only element of a list.
 #' @param replicate_type (character) name of the sampling method to be used.
-#' Options are "bootstrap" or "jackknife". Default = "bootstrap".
-#' @param bootstrap_percentage (numeric) percentage of data to be bootstrapped
+#' Options are "bootstrap", "jackknife", or "subsample". Default = "subsample".
+#' @param percentage (numeric) percentage of data to be bootstrapped
 #' for each replicate. Default = 50. Valid if \code{replicates} > 1 and
 #' \code{replicate_type} = "bootstrap".
 #'
 #' @usage
-#' data_subsample(data, replicates = 1, replicate_type = "bootstrap",
-#'                bootstrap_percentage = 50)
+#' data_subsample(data, replicates = 1, replicate_type = "subsample",
+#'                percentage = 50)
 #'
 #' @export
 #'
@@ -25,7 +25,7 @@
 #'
 #' # subsampling by bootstrap
 #' subsamples <- data_subsample(data = occurrences[, -1], replicates = 5,
-#'                              replicate_type = "bootstrap", bootstrap_percentage = 70)
+#'                              replicate_type = "bootstrap", percentage = 70)
 #'
 #' lapply(subsamples, head)
 #' lapply(subsamples, dim)
@@ -37,8 +37,8 @@
 #' lapply(subsamples1, head)
 #' lapply(subsamples1, dim)
 
-data_subsample <- function(data, replicates = 1, replicate_type = "bootstrap",
-                           bootstrap_percentage = 50) {
+data_subsample <- function(data, replicates = 1, replicate_type = "subsample",
+                           percentage = 75) {
   # -----------
   # detecting potential errors
   if (missing(data)) {
@@ -53,16 +53,17 @@ data_subsample <- function(data, replicates = 1, replicate_type = "bootstrap",
   } else {
     ndata <- nrow(data)
 
-    if (replicate_type == "bootstrap" | replicate_type == "jackknife") {
+    if (replicate_type %in% c("bootstrap", "jackknife", "subsample")) {
 
       if (replicate_type == "bootstrap") {
-        part <- round(ndata * bootstrap_percentage / 100)
+        part <- round(ndata * percentage / 100)
         samples <- lapply(1:replicates, function(x) {
           set.seed(x)
-          data[sample(ndata, part, replace = T), ]
+          data[sample(ndata, part, replace = TRUE), ]
         })
+      }
 
-      } else {
+      if (replicate_type == "jackknife") {
         if (replicates > ndata) {
           warning(paste0("Maximum number of replicates under \"jackknife\" replicate_type",
                          "\ncannot be > ", ndata, ", only ", ndata, " replicates will be produced."))
@@ -74,8 +75,16 @@ data_subsample <- function(data, replicates = 1, replicate_type = "bootstrap",
         })
       }
 
+      if (replicate_type == "subsample") {
+        part <- round(ndata * percentage / 100)
+        samples <- lapply(1:replicates, function(x) {
+          set.seed(x)
+          data[sample(ndata, part, replace = FALSE), ]
+        })
+      }
+
     } else {
-      stop("Argument replicate_type needs to be either \"bootstrap\" or \"jackknife\".")
+      stop("'replicate_type' must be: 'bootstrap', 'jackknife', or 'subsample'.")
     }
   }
 
